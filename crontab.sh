ebd923fab433d475395c01eb8ead7b05016a1559
karthikeyan.jayakuma <karthikeyan.jayakumar@gamesys.ee>
2022-09-06 16:29:35 +0300

    update corntab

--- crontab.sh
+++ crontab.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+repository="git@github.com:goldenratio/coolbet-stats.git"
+
+echo $tmp_dir
+git clone $repository $tmp_dir
+
+cd $tmp_dir
+npm i
+
 node ./src/index.js
-node deploy.js
+node ./deploy.js
+
+rm -rf $tmp_dir
